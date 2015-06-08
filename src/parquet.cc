4f21cb16d2015c7eaf19078861d538222080849b
Konstantin Knizhnik <knizhnik@garret.ru>
2015-06-08 16:59:04 +0300

    Add GZIP compression

--- src/parquet.cc
+++ src/parquet.cc
@@ -86,6 +86,12 @@ ColumnReader::ColumnReader(const ColumnMetaData* metadata,
     case CompressionCodec::SNAPPY:
       decompressor_.reset(new SnappyCodec());
       break;
+    case CompressionCodec::LZO:
+      decompressor_.reset(new Lz4Codec());
+      break;
+    case CompressionCodec::GZIP:
+      decompressor_.reset(new GZipCodec());
+      break;
     default:
       ParquetException::NYI("Reading compressed data");
   }
