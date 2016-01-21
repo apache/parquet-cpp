15294f4c43b7389b84e67d09204fbe92c4b0f843
Kashif Rasul <kashif.rasul@gmail.com>
2016-01-21 22:31:02 +0100

    updated thrift version to 0.9.3

--- thirdparty/versions.sh
+++ thirdparty/versions.sh
@@ -6,6 +6,6 @@ SNAPPY_VERSION=1.1.3
 SNAPPY_URL="https://github.com/google/snappy/releases/download/${SNAPPY_VERSION}/snappy-${SNAPPY_VERSION}.tar.gz"
 SNAPPY_BASEDIR=snappy-$SNAPPY_VERSION
 
-THRIFT_VERSION=0.9.1
+THRIFT_VERSION=0.9.3
 THRIFT_URL="http://archive.apache.org/dist/thrift/${THRIFT_VERSION}/thrift-${THRIFT_VERSION}.tar.gz"
 THRIFT_BASEDIR=thrift-$THRIFT_VERSION
