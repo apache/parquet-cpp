d044dd71fa135aa58cdd10f4abc6a422965dbb9b
Konstantin Knizhnik <knizhnik@garret.ru>
2016-01-29 11:44:59 +0300

    Remove trailing whitespaces

--- src/parquet/compression/gzip-codec.cc
+++ src/parquet/compression/gzip-codec.cc
@@ -29,28 +29,28 @@ void GZipCodec::Decompress(int input_len, const uint8_t* input,
 	stream.next_out = reinterpret_cast<Bytef*>(output_buffer);
 	stream.avail_out = output_len;
 	int rc = inflateInit2(&stream, 16+MAX_WBITS);
-	if (rc != Z_OK) 
+	if (rc != Z_OK)
 	{
 		throw ParquetException("zlib internal error.");
 	}
 	rc = inflate(&stream, Z_FINISH);
-	if (rc == Z_STREAM_END) 
+	if (rc == Z_STREAM_END)
 	{
 		rc = inflateEnd(&stream);
 	}
-	if (rc != Z_OK) 
+	if (rc != Z_OK)
 	{
 		throw ParquetException("Corrupt gzip compressed data.");
 	}
 }
 
-int GZipCodec::MaxCompressedLen(int input_len, const uint8_t* input) 
+int GZipCodec::MaxCompressedLen(int input_len, const uint8_t* input)
 {
 	return compressBound(input_len);
 }
 
 int GZipCodec::Compress(int input_len, const uint8_t* input,
-						int output_buffer_len, uint8_t* output_buffer) 
+						int output_buffer_len, uint8_t* output_buffer)
 {
 	uLongf dstLen = output_buffer_len;
 	int rc = compress2(reinterpret_cast<Bytef*>(output_buffer), &dstLen, reinterpret_cast<const Bytef*>(input), input_len, 1);
