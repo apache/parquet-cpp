d0c0837e48ea7d9c7d46eed42b64b2c90b0080cf
Konstantin Knizhnik <knizhnik@garret.ru>
2016-01-29 11:15:33 +0300

    Fix code style issues

--- src/parquet/compression/gzip-codec.cc
+++ src/parquet/compression/gzip-codec.cc
@@ -12,42 +12,49 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "codec.h"
+#include "parquet/compression/codec.h"
 
 #include <zlib.h>
 
-using namespace parquet_cpp;
+namespace parquet_cpp {
 
 void GZipCodec::Decompress(int input_len, const uint8_t* input,
-      int output_len, uint8_t* output_buffer) 
+						   int output_len, uint8_t* output_buffer)
 {
-    z_stream stream;
-    stream.zalloc = (alloc_func)0;
-    stream.zfree = (free_func)0;
-    stream.next_in = (Bytef *)input;
-    stream.avail_in = (uInt)input_len;
-    stream.next_out = (Bytef*)output_buffer;
-    stream.avail_out = output_len;
-    int rc = inflateInit2(&stream, 16+MAX_WBITS);
-    if (rc != Z_OK) { 
-        throw ParquetException("zlib internal error.");
-    }
-    rc = inflate(&stream, Z_FINISH); 
-    if (rc == Z_STREAM_END) {
-        rc = inflateEnd(&stream);
-    }
-    if (rc != Z_OK) {
-        throw ParquetException("Corrupt gzip compressed data.");
-    }
+	z_stream stream;
+	stream.zalloc = reinterpret_cast<alloc_func>0;
+	stream.zfree = reinterpret_cast<free_func>0;
+	stream.next_in = reinterpret_cast<Bytef *>input;
+	stream.avail_in = reinterpret_cast<uInt>input_len;
+	stream.next_out = reinterpret_cast<Bytef*>output_buffer;
+	stream.avail_out = output_len;
+	int rc = inflateInit2(&stream, 16+MAX_WBITS);
+	if (rc != Z_OK) 
+	{
+		throw ParquetException("zlib internal error.");
+	}
+	rc = inflate(&stream, Z_FINISH);
+	if (rc == Z_STREAM_END) 
+	{
+		rc = inflateEnd(&stream);
+	}
+	if (rc != Z_OK) 
+	{
+		throw ParquetException("Corrupt gzip compressed data.");
+	}
 }
 
-int GZipCodec::MaxCompressedLen(int input_len, const uint8_t* input) {
-  return compressBound(input_len);
+int GZipCodec::MaxCompressedLen(int input_len, const uint8_t* input) 
+{
+	return compressBound(input_len);
 }
 
 int GZipCodec::Compress(int input_len, const uint8_t* input,
-    int output_buffer_len, uint8_t* output_buffer) {
-    uLongf dstLen = output_buffer_len;    
-    int rc = compress2((Bytef*)output_buffer, &dstLen, (Bytef*)input, input_len, 1);
-    return rc == Z_OK ? dstLen : input_len;
+						int output_buffer_len, uint8_t* output_buffer) 
+{
+	uLongf dstLen = output_buffer_len;
+	int rc = compress2(reinterpret_cast<Bytef*>output_buffer, &dstLen, reinterpret_cast<Bytef*>input, input_len, 1);
+	return rc == Z_OK ? dstLen : input_len;
 }
+
+} // namespace parquet_cpp
