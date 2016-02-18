fed3317274ccab4bb544a2baa0d120a875630a05
Wes McKinney <wesm@apache.org>
2016-02-18 15:54:32 -0800

    PARQUET-468: Use thirdparty Thrift compiler to compile parquet.thrift at make time
    This also adds a `#define` working around the googletest incompatibility described in PARQUET-470.
    
    Author: Wes McKinney <wesm@apache.org>
    
    Closes #55 from wesm/PARQUET-468 and squashes the following commits:
    
    e0338df [Wes McKinney] Auto-generate Thrift C++ bindings from environment Thrift compiler. Add #define workaround for Thrift >= 0.9.2 std::tuple conflict with googletest

--- thirdparty/build_thirdparty.sh
+++ thirdparty/build_thirdparty.sh
@@ -98,6 +98,8 @@ if [ -n "$F_ALL" -o -n "$F_THRIFT" ]; then
     # such a way that ./configure can find them
     cd $TP_DIR/$THRIFT_BASEDIR
     ./configure CXXFLAGS='-fPIC' --without-qt4 --without-c_glib --without-csharp --without-java --without-erlang --without-nodejs --without-lua --without-python --without-perl --without-php --without-php_extension --without-ruby --without-haskell --without-go --without-d --with-cpp --prefix=$PREFIX
+
+	# This must be removed with Thrift 0.9.3, but required for Thrift 0.9.1
 	make clean
     make install
   fi
