41c1e6887cc53d43d48c7d327ed04ee4dd1a57b4
Wes McKinney <wesm@apache.org>
2016-02-29 16:17:16 -0800

    PARQUET-520: Add MemoryMapSource and add unit tests for both it and LocalFileSource
    I also added the `file_descriptor` API so that we can verify that dtors elsewhere successfully close open files. Closes #56
    
    Author: Wes McKinney <wesm@apache.org>
    
    Closes #66 from wesm/PARQUET-520 and squashes the following commits:
    
    9d638ba [Wes McKinney] Add memory-mapping option to ParquetFileReader::OpenFile. Add --no-memory-map flag to parquet_reader
    6389683 [Wes McKinney] Add Read API tests
    dbf6a45 [Wes McKinney] Test some failure modes for LocalFileSource / MemoryMapSource
    01a7d64 [Wes McKinney] Add a MemoryMapSource and use this by default for SerializedFileReader

--- example/parquet_reader.cc
+++ example/parquet_reader.cc
@@ -24,26 +24,30 @@ using namespace parquet_cpp;
 
 int main(int argc, char** argv) {
   if (argc > 3) {
-    std::cerr << "Usage: parquet_reader [--only-stats] <file>"
+    std::cerr << "Usage: parquet_reader [--only-stats] [--no-memory-map] <file>"
               << std::endl;
     return -1;
   }
 
   std::string filename;
   bool print_values = true;
+  bool memory_map = true;
 
   // Read command-line options
   char *param, *value;
   for (int i = 1; i < argc; i++) {
-    if ( (param = std::strstr(argv[i], "--only-stats")) ) {
+    if ((param = std::strstr(argv[i], "--only-stats"))) {
       print_values = false;
+    } else if ((param = std::strstr(argv[i], "--no-memory-map"))) {
+      memory_map = false;
     } else {
       filename = argv[i];
     }
   }
 
   try {
-    std::unique_ptr<ParquetFileReader> reader = ParquetFileReader::OpenFile(filename);
+    std::unique_ptr<ParquetFileReader> reader = ParquetFileReader::OpenFile(filename,
+        memory_map);
     reader->DebugPrint(std::cout, print_values);
   } catch (const std::exception& e) {
     std::cerr << "Parquet error: "
