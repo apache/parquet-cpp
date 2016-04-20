63c06f077e4aa3fe861b2a82d385b432f2768425
Uwe L. Korn <uwelk@xhochy.com>
2016-04-20 21:33:58 +0200

    Refactored unit tests

--- src/parquet/column/column-writer-test.cc
+++ src/parquet/column/column-writer-test.cc
@@ -50,10 +50,15 @@ class TestPrimitiveWriter : public ::testing::Test {
   }
 
   void SetUp() {
+    values_out.resize(100);
+    definition_levels_out.resize(100);
+    repetition_levels_out.resize(100);
+
     SetUpSchemaRequiredNonRepeated();
   }
 
-  std::unique_ptr<Int64Reader> BuildReader(const std::shared_ptr<Buffer>& buffer) {
+  std::unique_ptr<Int64Reader> BuildReader() {
+    auto buffer = sink_->GetBuffer();
     std::unique_ptr<InMemoryInputStream> source(new InMemoryInputStream(buffer));
     std::unique_ptr<SerializedPageReader> page_reader(
         new SerializedPageReader(std::move(source), Compression::UNCOMPRESSED));
@@ -61,76 +66,82 @@ class TestPrimitiveWriter : public ::testing::Test {
         new Int64Reader(schema.get(), std::move(page_reader)));
   }
 
-  std::unique_ptr<Int64Writer> BuildWriter(OutputStream* sink) {
+  std::unique_ptr<Int64Writer> BuildWriter() {
+    sink_.reset(new InMemoryOutputStream());
     std::unique_ptr<SerializedPageWriter> pager(
-        new SerializedPageWriter(sink, Compression::UNCOMPRESSED));
+        new SerializedPageWriter(sink_.get(), Compression::UNCOMPRESSED));
     return std::unique_ptr<Int64Writer>(new Int64Writer(schema.get(), std::move(pager)));
   }
+
+  void ReadColumn() {
+    auto reader = BuildReader();
+    reader->ReadBatch(values_out.size(), definition_levels_out.data(),
+        repetition_levels_out.data(), values_out.data(), &values_read);
+  }
+
+ protected:
+  int64_t values_read;
+  
+  // Output buffers
+  std::vector<int64_t> values_out;
+  std::vector<int16_t> definition_levels_out;
+  std::vector<int16_t> repetition_levels_out;
 
  private:
   NodePtr node;
   std::shared_ptr<ColumnDescriptor> schema;
+  std::unique_ptr<InMemoryOutputStream> sink_;
 };
 
-TEST_F(TestPrimitiveWriter, WriteReadLoopSinglePage) {
-  // Small dataset that should fit inside a single page
-  std::vector<int64_t> values(100);
-  std::fill(values.begin(), values.end(), 128);
-  std::vector<int16_t> definition_levels(100);
-  std::fill(definition_levels.begin(), definition_levels.end(), 1);
-  definition_levels[1] = 0;
-  std::vector<int16_t> repetition_levels(100);
-  std::fill(repetition_levels.begin(), repetition_levels.end(), 0);
-
-  // Output buffers
-  std::vector<int64_t> values_out(100);
-  std::vector<int16_t> definition_levels_out(100);
-  std::vector<int16_t> repetition_levels_out(100);
+TEST_F(TestPrimitiveWriter, RequiredNonRepeated) {
+  std::vector<int64_t> values(100, 128);
 
   // Test case 1: required and non-repeated, so no definition or repetition levels
-  std::unique_ptr<InMemoryOutputStream> sink(new InMemoryOutputStream());
-  std::unique_ptr<Int64Writer> writer = BuildWriter(sink.get());
+  std::unique_ptr<Int64Writer> writer = BuildWriter();
   writer->WriteBatch(values.size(), nullptr, nullptr, values.data());
   writer->Close();
 
-  std::unique_ptr<Int64Reader> reader = BuildReader(sink->GetBuffer());
-  int64_t values_read = 0;
-  reader->ReadBatch(values.size(), definition_levels_out.data(),
-      repetition_levels_out.data(), values_out.data(), &values_read);
+  ReadColumn();
   ASSERT_EQ(values_read, 100);
   ASSERT_EQ(values_out, values);
+}
 
-  // Test case 2: optional and non-repeated, with definition levels
+TEST_F(TestPrimitiveWriter, OptionalNonRepeated) {
+  // Optional and non-repeated, with definition levels
   // but no repetition levels
   SetUpSchemaOptionalNonRepeated();
-  sink.reset(new InMemoryOutputStream());
-  writer = BuildWriter(sink.get());
+  
+  std::vector<int64_t> values(100, 128);
+  std::vector<int16_t> definition_levels(100, 1);
+  definition_levels[1] = 0;
+  std::vector<int64_t> values_expected(99, 128);
+  
+  auto writer = BuildWriter();
   writer->WriteBatch(values.size(), definition_levels.data(), nullptr, values.data());
   writer->Close();
 
-  reader = BuildReader(sink->GetBuffer());
-  values_read = 0;
-  reader->ReadBatch(values.size(), definition_levels_out.data(),
-      repetition_levels_out.data(), values_out.data(), &values_read);
+  ReadColumn();
   ASSERT_EQ(values_read, 99);
-  std::vector<int64_t> values_expected(99);
-  std::fill(values_expected.begin(), values_expected.end(), 128);
   values_out.resize(99);
   ASSERT_EQ(values_out, values_expected);
+}
 
-  // Test case 3: optional and repeated, so definition and repetition levels
+TEST_F(TestPrimitiveWriter, OptionalRepeated) {
+  // Optional and repeated, so definition and repetition levels
   SetUpSchemaOptionalRepeated();
-  sink.reset(new InMemoryOutputStream());
-  writer = BuildWriter(sink.get());
+
+  std::vector<int64_t> values(100, 128);
+  std::vector<int16_t> definition_levels(100, 1);
+  definition_levels[1] = 0;
+  std::vector<int16_t> repetition_levels(100, 0);
+  std::vector<int64_t> values_expected(99, 128);
+
+  auto writer = BuildWriter();
   writer->WriteBatch(values.size(), definition_levels.data(),
       repetition_levels.data(), values.data());
   writer->Close();
 
-  reader = BuildReader(sink->GetBuffer());
-  values_read = 0;
-  values_out.resize(100);
-  reader->ReadBatch(values.size(), definition_levels_out.data(),
-      repetition_levels_out.data(), values_out.data(), &values_read);
+  ReadColumn();
   ASSERT_EQ(values_read, 99);
   values_out.resize(99);
   ASSERT_EQ(values_out, values_expected);
