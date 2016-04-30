6c2e7cfa9774dff8c4b9f11d291ef9a1edbe3925
Wes McKinney <wesm@apache.org>
2016-04-30 14:55:26 -0700

    Adapt clang-tidy / clang-format tools from Apache Arrow C++ codebase

---
+++ ci/travis_script_cpp.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+: ${CPP_BUILD_DIR=$TRAVIS_BUILD_DIR/cpp-build}
+
+pushd $CPP_BUILD_DIR
+
+make lint
+if [ $TRAVIS_OS_NAME == "linux" ]; then
+  make check-format
+  make check-clang-tidy
+fi
+
+if [ $TRAVIS_OS_NAME == "linux" ]; then
+  make -j4 || exit 1
+  ctest || { cat $TRAVIS_BUILD_DIR/parquet-build/Testing/Temporary/LastTest.log; exit 1; }
+  sudo pip install cpp_coveralls
+  export PARQUET_ROOT=$TRAVIS_BUILD_DIR
+  $TRAVIS_BUILD_DIR/ci/upload_coverage.sh
+else
+  make -j4 || exit 1
+  ctest || { cat $TRAVIS_BUILD_DIR/parquet-build/Testing/Temporary/LastTest.log; exit 1; }
+fi
+
+popd
