f53f5703992ecb2e30ae4cc2825e2cae0e6fb2f0
Wes McKinney <wesm@apache.org>
2016-05-17 12:43:13 -0700

    PARQUET-618: Automate posting conda build artifacts to anaconda.org
    This will enable Python or other conda users to be able to install portable `libparquet` dev binaries into their conda environments (e.g. for `libarrow_parquet` to depend on).
    
    Author: Wes McKinney <wesm@apache.org>
    
    Closes #110 from wesm/PARQUET-618 and squashes the following commits:
    
    da91ad2 [Wes McKinney] Use Travis build number
    eaeb887 [Wes McKinney] Conda build CI script that automatically posts artifacts to apache/dev subchannel on anaconda.org. Toolchain fixes / work around conda-build bugs

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -12,6 +12,19 @@ export SNAPPY_HOME=$PREFIX
 export THRIFT_HOME=$PREFIX
 export ZLIB_HOME=$PREFIX
 
+if [ "$(uname)" == "Darwin" ]; then
+  # C++11 finagling for Mac OSX
+  export CC=clang
+  export CXX=clang++
+  export MACOSX_VERSION_MIN="10.7"
+  CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
+  export LINKFLAGS="${LDFLAGS}"
+  export MACOSX_DEPLOYMENT_TARGET=10.7
+fi
+
 cd ..
 
 rm -rf conda-build
