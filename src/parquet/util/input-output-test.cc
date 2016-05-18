612a2f2cca3251dcebc1c3662e378f8f54457c5b
Uwe L. Korn <uwelk@xhochy.com>
2016-05-18 09:23:51 +0200

    Check that the correct data was written

--- src/parquet/util/input-output-test.cc
+++ src/parquet/util/input-output-test.cc
@@ -223,15 +223,22 @@ class TestFileWriter : public ::testing::Test {
 
  protected:
   std::string test_path_;
-  uint8_t data_[4] = {1, 2, 3, 4};
+  uint8_t test_data_[4] = {1, 2, 3, 4};
 };
 
 TEST_F(TestFileWriter, Write) {
   LocalFileOutputStream sink(test_path_);
   ASSERT_EQ(0, sink.Tell());
-  sink.Write(data_, 4);
+  sink.Write(test_data_, 4);
   ASSERT_EQ(4, sink.Tell());
   sink.Close();
+
+  // Check that the correct content was written
+  LocalFileSource source;
+  source.Open(test_path_);
+  std::shared_ptr<Buffer> buffer = source.Read(4);
+  ASSERT_EQ(4, buffer->size());
+  ASSERT_EQ(0, memcmp(test_data_, buffer->data(), 4));
 }
 
 }  // namespace parquet
