f334a8b97cd2499799cc74262a52191d04692773
Wes McKinney <wesm@apache.org>
2016-06-21 13:31:42 -0700

    PARQUET-635: Use conda-forge toolchain, statically link libstdc++ on Linux
    This will avoid portability issues across different Linux environments.
    
    Author: Wes McKinney <wesm@apache.org>
    
    Closes #123 from wesm/PARQUET-635 and squashes the following commits:
    
    63039a1 [Wes McKinney] Use conda-forge toolchain, statically link libstdc++

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -3,6 +3,14 @@
 set -e
 set -x
 
+# FIXME: This is a hack to make sure the environment is activated.
+# The reason this is required is due to the conda-build issue
+# mentioned below.
+#
+# https://github.com/conda/conda-build/issues/910
+#
+source activate "${CONDA_DEFAULT_ENV}"
+
 cd $RECIPE_DIR
 
 # Build dependencies
@@ -11,19 +19,6 @@ export BOOST_ROOT=$PREFIX
 export SNAPPY_HOME=$PREFIX
 export THRIFT_HOME=$PREFIX
 export ZLIB_HOME=$PREFIX
-
-if [ "$(uname)" == "Darwin" ]; then
-  # C++11 finagling for Mac OSX
-  export CC=clang
-  export CXX=clang++
-  export MACOSX_VERSION_MIN="10.7"
-  CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
-  export LINKFLAGS="${LDFLAGS}"
-  export MACOSX_DEPLOYMENT_TARGET=10.7
-fi
 
 cd ..
 
@@ -50,9 +45,16 @@ export PARQUET_INSECURE_CURL=1
 source thirdparty/versions.sh
 export GTEST_HOME=`pwd`/thirdparty/$GTEST_BASEDIR
 
+if [ `uname` == Linux ]; then
+    SHARED_LINKER_FLAGS='-static-libstdc++'
+elif [ `uname` == Darwin ]; then
+    SHARED_LINKER_FLAGS=''
+fi
+
 cmake \
     -DCMAKE_BUILD_TYPE=release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_SHARED_LINKER_FLAGS=$SHARED_LINKER_FLAGS \
     -DPARQUET_BUILD_BENCHMARKS=off \
     ..
 
