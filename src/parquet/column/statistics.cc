f17926da901f5cf97c14538f6bca0ad8a4e65c2e
fscheibner <florian.scheibner@snowflake.net>
2016-10-07 12:14:44 -0700

    fix typedrowgroup

--- src/parquet/column/statistics.cc
+++ src/parquet/column/statistics.cc
@@ -67,6 +67,25 @@ TypedRowGroupStatistics<DType>::TypedRowGroupStatistics(const ColumnDescriptor* 
   has_min_max_ = !encoded_min.empty() && !encoded_max.empty();
 }
 
+template <>
+TypedRowGroupStatistics<ByteArrayType>::TypedRowGroupStatistics(const ColumnDescriptor* schema,
+                                                        const std::string& encoded_min, const std::string& encoded_max, int64_t num_values,
+                                                        int64_t null_count, int64_t distinct_count, MemoryAllocator* allocator)
+   : allocator_(allocator), min_buffer_(0, allocator_), max_buffer_(0, allocator_) {
+  IncrementNumValues(num_values);
+  IncrementNullCount(null_count);
+  IncrementDistinctCount(distinct_count);
+
+  SetDescr(schema);
+
+  min_.len = encoded_min.size();
+  min_.ptr = reinterpret_cast<const uint8_t*>(encoded_min.c_str());
+  max_.len = encoded_max.size();
+  max_.ptr = reinterpret_cast<const uint8_t*>(encoded_max.c_str());
+
+  has_min_max_ = !encoded_min.empty() && !encoded_max.empty();
+}
+
 template <typename DType>
 bool TypedRowGroupStatistics<DType>::HasMinMax() const {
   return has_min_max_;
