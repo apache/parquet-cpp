c7061d9ef66e4512741ef04a7af13b7ab7af6139
Deepak Majeti <deepak.majeti@hpe.com>
2016-11-08 11:45:51 -0500

    review comments

--- examples/reader-writer.cc
+++ examples/reader-writer.cc
@@ -52,67 +52,45 @@ using parquet::LogicalType;
 using parquet::schema::PrimitiveNode;
 using parquet::schema::GroupNode;
 
-class ParquetWriter {
- public:
-  ParquetWriter() {
-    ostream_.reset(new parquet::LocalFileOutputStream(PARQUET_FILENAME));
-  }
+static std::shared_ptr<GroupNode> SetupSchema() {
+  parquet::schema::NodeVector fields;
+  // Create a primitive node named 'boolean_field' with type:BOOLEAN,
+  // repetition:REQUIRED
+  fields.push_back(PrimitiveNode::Make(
+      "boolean_field", Repetition::REQUIRED, Type::BOOLEAN, LogicalType::NONE));
 
-  void SetupSchema() {
-    parquet::schema::NodeVector fields;
-    // Create a primitive node named 'boolean_field' with type:BOOLEAN,
-    // repetition:REQUIRED
-    fields.push_back(PrimitiveNode::Make(
-        "boolean_field", Repetition::REQUIRED, Type::BOOLEAN, LogicalType::NONE));
+  // Create a primitive node named 'int32_field' with type:INT32, repetition:REQUIRED,
+  // logical type:TIME_MILLIS
+  fields.push_back(PrimitiveNode::Make(
+      "int32_field", Repetition::REQUIRED, Type::INT32, LogicalType::TIME_MILLIS));
 
-    // Create a primitive node named 'int32_field' with type:INT32, repetition:REQUIRED,
-    // logical type:TIME_MILLIS
-    fields.push_back(PrimitiveNode::Make(
-        "int32_field", Repetition::REQUIRED, Type::INT32, LogicalType::TIME_MILLIS));
+  // Create a primitive node named 'int64_field' with type:INT64, repetition:REPEATED
+  fields.push_back(PrimitiveNode::Make(
+      "int64_field", Repetition::REPEATED, Type::INT64, LogicalType::NONE));
 
-    // Create a primitive node named 'int64_field' with type:INT64, repetition:REPEATED
-    fields.push_back(PrimitiveNode::Make(
-        "int64_field", Repetition::REPEATED, Type::INT64, LogicalType::NONE));
+  fields.push_back(PrimitiveNode::Make(
+      "int96_field", Repetition::REQUIRED, Type::INT96, LogicalType::NONE));
 
-    fields.push_back(PrimitiveNode::Make(
-        "int96_field", Repetition::REQUIRED, Type::INT96, LogicalType::NONE));
+  fields.push_back(PrimitiveNode::Make(
+      "float_field", Repetition::REQUIRED, Type::FLOAT, LogicalType::NONE));
 
-    fields.push_back(PrimitiveNode::Make(
-        "float_field", Repetition::REQUIRED, Type::FLOAT, LogicalType::NONE));
+  fields.push_back(PrimitiveNode::Make(
+      "double_field", Repetition::REQUIRED, Type::DOUBLE, LogicalType::NONE));
 
-    fields.push_back(PrimitiveNode::Make(
-        "double_field", Repetition::REQUIRED, Type::DOUBLE, LogicalType::NONE));
+  // Create a primitive node named 'ba_field' with type:BYTE_ARRAY, repetition:OPTIONAL
+  fields.push_back(PrimitiveNode::Make(
+      "ba_field", Repetition::OPTIONAL, Type::BYTE_ARRAY, LogicalType::NONE));
 
-    // Create a primitive node named 'ba_field' with type:BYTE_ARRAY, repetition:OPTIONAL
-    fields.push_back(PrimitiveNode::Make(
-        "ba_field", Repetition::OPTIONAL, Type::BYTE_ARRAY, LogicalType::NONE));
+  // Create a primitive node named 'flba_field' with type:FIXED_LEN_BYTE_ARRAY,
+  // repetition:REQUIRED, field_length = FIXED_LENGTH
+  fields.push_back(PrimitiveNode::Make("flba_field", Repetition::REQUIRED,
+      Type::FIXED_LEN_BYTE_ARRAY, LogicalType::NONE, FIXED_LENGTH));
 
-    // Create a primitive node named 'flba_field' with type:FIXED_LEN_BYTE_ARRAY,
-    // repetition:REQUIRED, field_length = FIXED_LENGTH
-    fields.push_back(PrimitiveNode::Make("flba_field", Repetition::REQUIRED,
-        Type::FIXED_LEN_BYTE_ARRAY, LogicalType::NONE, FIXED_LENGTH));
-
-    // Create a GroupNode named 'schema' using the primitive nodes defined above
-    // This GroupNode is the root node of the schema tree
-    schema_ = std::static_pointer_cast<GroupNode>(
-        GroupNode::Make("schema", Repetition::REQUIRED, fields));
-  }
-
-  parquet::ParquetFileWriter* GetWriter() {
-    if (!writer_) {
-      // Create a parquet file writer using the schema and local file output stream.
-      writer_ = parquet::ParquetFileWriter::Open(ostream_, schema_);
-    }
-    return writer_.get();
-  }
-
-  void Close() { ostream_->Close(); }
-
- private:
-  std::shared_ptr<parquet::schema::GroupNode> schema_;
-  std::shared_ptr<parquet::LocalFileOutputStream> ostream_;
-  std::unique_ptr<parquet::ParquetFileWriter> writer_;
-};
+  // Create a GroupNode named 'schema' using the primitive nodes defined above
+  // This GroupNode is the root node of the schema tree
+  return std::static_pointer_cast<GroupNode>(
+      GroupNode::Make("schema", Repetition::REQUIRED, fields));
+}
 
 int main(int argc, char** argv) {
   /**********************************************************************************
@@ -122,14 +100,16 @@ int main(int argc, char** argv) {
   // parquet::OPTIONAL fields require only definition level values
   // parquet::REPEATED fields require both definition and repetition level values
   try {
-    // Create a ParquetWriter instance
-    auto parquet_writer = std::unique_ptr<ParquetWriter>(new ParquetWriter());
+    // Create a local file output stream instance.
+    std::shared_ptr<parquet::OutputStream> out_file =
+        std::make_shared<parquet::LocalFileOutputStream>(PARQUET_FILENAME);
 
-    // Setup the schema
-    parquet_writer->SetupSchema();
+    // Setup the parquet schema
+    std::shared_ptr<GroupNode> schema = SetupSchema();
 
-    // Get a handle to the ParquetFileWriter
-    parquet::ParquetFileWriter* file_writer = parquet_writer->GetWriter();
+    // Create a ParquetFileWriter instance
+    std::shared_ptr<parquet::ParquetFileWriter> file_writer =
+        parquet::ParquetFileWriter::Open(out_file, schema);
 
     // Append a RowGroup with a specific number of rows.
     parquet::RowGroupWriter* rg_writer =
@@ -142,7 +122,6 @@ int main(int argc, char** argv) {
       bool value = ((i % 2) == 0) ? true : false;
       bool_writer->WriteBatch(1, nullptr, nullptr, &value);
     }
-    bool_writer->Close();
 
     // Write the Int32 column
     parquet::Int32Writer* int32_writer =
@@ -151,7 +130,6 @@ int main(int argc, char** argv) {
       int32_t value = i;
       int32_writer->WriteBatch(1, nullptr, nullptr, &value);
     }
-    int32_writer->Close();
 
     // Write the Int64 column. Each row has repeats twice.
     parquet::Int64Writer* int64_writer =
@@ -166,7 +144,6 @@ int main(int argc, char** argv) {
       }
       int64_writer->WriteBatch(1, &definition_level, &repetition_level, &value);
     }
-    int64_writer->Close();
 
     // Write the INT96 column.
     parquet::Int96Writer* int96_writer =
@@ -178,7 +155,6 @@ int main(int argc, char** argv) {
       value.value[2] = i + 2;
       int96_writer->WriteBatch(1, nullptr, nullptr, &value);
     }
-    int96_writer->Close();
 
     // Write the Float column
     parquet::FloatWriter* float_writer =
@@ -187,7 +163,6 @@ int main(int argc, char** argv) {
       float value = i * 1.1;
       float_writer->WriteBatch(1, nullptr, nullptr, &value);
     }
-    float_writer->Close();
 
     // Write the Double column
     parquet::DoubleWriter* double_writer =
@@ -196,7 +171,6 @@ int main(int argc, char** argv) {
       double value = i * 1.1111111;
       double_writer->WriteBatch(1, nullptr, nullptr, &value);
     }
-    double_writer->Close();
 
     // Write the ByteArray column. Make every alternate values NULL
     parquet::ByteArrayWriter* ba_writer =
@@ -217,7 +191,6 @@ int main(int argc, char** argv) {
         ba_writer->WriteBatch(1, &definition_level, nullptr, nullptr);
       }
     }
-    ba_writer->Close();
 
     // Write the FixedLengthByteArray column
     parquet::FixedLenByteArrayWriter* flba_writer =
@@ -230,16 +203,12 @@ int main(int argc, char** argv) {
 
       flba_writer->WriteBatch(1, nullptr, nullptr, &value);
     }
-    flba_writer->Close();
-
-    // Close the RowGroup Writer
-    rg_writer->Close();
 
     // Close the ParquetFileWriter
     file_writer->Close();
 
     // Write the bytes to file
-    parquet_writer->Close();
+    out_file->Close();
   } catch (const std::exception& e) {
     std::cerr << "Parquet write error: " << e.what() << std::endl;
     return -1;
