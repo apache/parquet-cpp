b89738a6882450ac1228b872f2b08205e60ca2cc
Uwe L. Korn <uwelk@xhochy.com>
2016-11-27 12:19:50 +0100

    Update arrow hash

--- thirdparty/versions.sh
+++ thirdparty/versions.sh
@@ -15,7 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-ARROW_VERSION="f082b17323354dc2af31f39c15c58b995ba08360"
+ARROW_VERSION="86f56a6073c3254487ede3aff1dc9d117d24adaf"
 ARROW_URL="https://github.com/apache/arrow/archive/${ARROW_VERSION}.tar.gz"
 ARROW_BASEDIR="arrow-${ARROW_VERSION}"
 
