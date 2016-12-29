f51611523d1fc45951acfff23d9ae3d1ff715289
Wes McKinney <wes.mckinney@twosigma.com>
2016-12-29 08:44:18 -0500

    Add public link libs to dependencies to avoid race conditions with external projects

--- cmake_modules/ThirdpartyToolchain.cmake
+++ cmake_modules/ThirdpartyToolchain.cmake
@@ -22,7 +22,7 @@ set(THRIFT_VERSION "0.9.1")
 
 # Brotli 0.5.2 does not install headers/libraries yet, but 0.6.0.dev does
 set(BROTLI_VERSION "5db62dcc9d386579609540cdf8869e95ad334bbd")
-set(ARROW_VERSION "4733ee876e1fddb8032fce1dc9e486d68904fbea")
+set(ARROW_VERSION "e15c6a0b3c05b5b42c204f34369d127182450ca0")
 
 # find boost headers and libs
 set(Boost_DEBUG TRUE)
