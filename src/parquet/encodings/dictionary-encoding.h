393d99aa44e1b0f8568d6b0a93272e56668360f4
Uwe L. Korn <uwelk@xhochy.com>
2017-01-11 21:06:25 +0100

    Explicitly mark overrides

--- src/parquet/encodings/dictionary-encoding.h
+++ src/parquet/encodings/dictionary-encoding.h
@@ -51,7 +51,7 @@ class DictionaryDecoder : public Decoder<Type> {
   // Perform type-specific initiatialization
   void SetDict(Decoder<Type>* dictionary);
 
-  virtual void SetData(int num_values, const uint8_t* data, int len) {
+  void SetData(int num_values, const uint8_t* data, int len) override {
     num_values_ = num_values;
     if (len == 0) return;
     uint8_t bit_width = *data;
@@ -60,7 +60,7 @@ class DictionaryDecoder : public Decoder<Type> {
     idx_decoder_ = RleDecoder(data, len, bit_width);
   }
 
-  virtual int Decode(T* buffer, int max_values) {
+  int Decode(T* buffer, int max_values) override {
     max_values = std::min(max_values, num_values_);
     int decoded_values = idx_decoder_.GetBatchWithDict(dictionary_, buffer, max_values);
     if (decoded_values != max_values) { ParquetException::EofException(); }
