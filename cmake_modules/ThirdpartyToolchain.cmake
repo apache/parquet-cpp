e3b7f58de45e87a0af379cac915313a19268b132
Korn, Uwe <Uwe.Korn@blue-yonder.com>
2017-01-27 14:09:16 +0100

    Update arrow hash

--- cmake_modules/ThirdpartyToolchain.cmake
+++ cmake_modules/ThirdpartyToolchain.cmake
@@ -22,7 +22,7 @@ set(THRIFT_VERSION "0.9.1")
 
 # Brotli 0.5.2 does not install headers/libraries yet, but 0.6.0.dev does
 set(BROTLI_VERSION "5db62dcc9d386579609540cdf8869e95ad334bbd")
-set(ARROW_VERSION "085c8754b0ab2da7fcd245fc88bc4de9a6806a4c")
+set(ARROW_VERSION "4226adfbc6b3dff10b3fe7a6691b30bcc94140bd")
 
 # find boost headers and libs
 set(Boost_DEBUG TRUE)
