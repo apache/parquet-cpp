f8573ebedbeb3498a3cb5b5234131f913713d6ec
Deepak Majeti <deepak.majeti@hpe.com>
2017-04-04 16:42:34 -0400

    PARQUET-945: Thrift static libraries are not used with recent patch
    Author: Deepak Majeti <deepak.majeti@hpe.com>
    
    Closes #290 from majetideepak/PARQUET-945 and squashes the following commits:
    
    875da78 [Deepak Majeti] remove thrift shared library detection
    0ce8623 [Deepak Majeti] Fix static library resolution

--- cmake_modules/FindThrift.cmake
+++ cmake_modules/FindThrift.cmake
@@ -25,7 +25,7 @@
 #  THRIFT_VERSION, version string of ant if found
 #  THRIFT_INCLUDE_DIR, where to find THRIFT headers
 #  THRIFT_CONTRIB_DIR, where contrib thrift files (e.g. fb303.thrift) are installed
-#  THRIFT_LIBS, THRIFT libraries
+#  THRIFT_STATIC_LIB, THRIFT static library
 #  THRIFT_FOUND, If false, do not try to use ant
 
 # prefer the thrift version supplied in THRIFT_HOME
@@ -48,15 +48,8 @@ find_path(THRIFT_CONTRIB_DIR share/fb303/if/fb303.thrift HINTS
   NO_DEFAULT_PATH
 )
 
-find_library(THRIFT_STATIC_LIB
+find_library(THRIFT_STATIC_LIB NAMES
   ${CMAKE_STATIC_LIBRARY_PREFIX}thrift${CMAKE_STATIC_LIBRARY_SUFFIX}
-  HINTS ${_thrift_roots}
-  NO_DEFAULT_PATH
-  PATH_SUFFIXES "lib/${CMAKE_LIBRARY_ARCHITECTURE}" "lib"
-)
-
-find_library(THRIFT_LIB NAMES
-  ${CMAKE_SHARED_LIBRARY_PREFIX}thrift${CMAKE_SHARED_LIBRARY_SUFFIX}
   HINTS ${_thrift_roots}
   NO_DEFAULT_PATH
   PATH_SUFFIXES "lib/${CMAKE_LIBRARY_ARCHITECTURE}" "lib"
@@ -68,9 +61,8 @@ find_program(THRIFT_COMPILER thrift HINTS
   PATH_SUFFIXES "bin"
 )
 
-if (THRIFT_LIB)
+if (THRIFT_STATIC_LIB)
   set(THRIFT_FOUND TRUE)
-  set(THRIFT_LIBS ${THRIFT_LIB})
   exec_program(${THRIFT_COMPILER}
     ARGS -version OUTPUT_VARIABLE THRIFT_VERSION RETURN_VALUE THRIFT_RETURN)
 else ()
@@ -84,12 +76,12 @@ if (THRIFT_FOUND)
 else ()
   message(STATUS "Thrift compiler/libraries NOT found. "
           "Thrift support will be disabled (${THRIFT_RETURN}, "
-          "${THRIFT_INCLUDE_DIR}, ${THRIFT_LIB})")
+          "${THRIFT_INCLUDE_DIR}, ${THRIFT_STATIC_LIB})")
 endif ()
 
 
 mark_as_advanced(
-  THRIFT_LIB
+  THRIFT_STATIC_LIB
   THRIFT_COMPILER
   THRIFT_INCLUDE_DIR
 )
