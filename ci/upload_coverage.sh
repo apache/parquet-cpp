9adbe6d2a067ea08be83fb90ab511dce231bbe45
Phillip Cloud <cpcloud@gmail.com>
2017-06-23 00:51:44 -0400

    PARQUET-1038: Key value metadata should be nullptr if not set
    Author: Phillip Cloud <cpcloud@gmail.com>
    
    Closes #357 from cpcloud/PARQUET-1038 and squashes the following commits:
    
    5fc6276 [Phillip Cloud] Ridiculous hack
    b7401e0 [Phillip Cloud] Pass specific gcov
    8c99235 [Phillip Cloud] Turn off haskell
    fbbc45e [Phillip Cloud] PARQUET-1038: Key value metadata should be nullptr if not set

--- ci/upload_coverage.sh
+++ ci/upload_coverage.sh
@@ -26,7 +26,9 @@ ls -l
 
 echo $PARQUET_ROOT
 
-coveralls --gcov-options '\-l' -r $PARQUET_ROOT \
+coveralls --gcov $(which gcov-4.9) \
+    --gcov-options '\-l' --root '' \
+    --include $PARQUET_ROOT \
     --exclude $PARQUET_ROOT/parquet-build/thirdparty \
     --exclude $PARQUET_ROOT/parquet-build/arrow_ep \
     --exclude $PARQUET_ROOT/parquet-build/brotli_ep \
@@ -38,5 +40,5 @@ coveralls --gcov-options '\-l' -r $PARQUET_ROOT \
     --exclude $PARQUET_ROOT/parquet-build/zlib_ep \
     --exclude $PARQUET_ROOT/parquet-build/zlib_ep-prefix \
     --exclude $PARQUET_ROOT/build \
-    --exclude /usr \
-    --exclude $PARQUET_ROOT/src/parquet/thrift
+    --exclude $PARQUET_ROOT/src/parquet/thrift \
+    --exclude /usr
