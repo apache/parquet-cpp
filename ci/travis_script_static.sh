0bbd073dc43fe1761c8fdf4b1829a151fdb51990
Deepak Majeti <deepak.majeti@hpe.com>
2017-07-09 11:06:57 -0400

    add env vars

--- ci/travis_script_static.sh
+++ ci/travis_script_static.sh
@@ -25,16 +25,23 @@ else
 fi
 
 export PARQUET_TEST_DATA=$TRAVIS_BUILD_DIR/data
+export SNAPPY_STATIC_LIB=$TRAVIS_BUILD_DIR/parquet-build/arrow_ep-prefix/src/arrow_ep-build/snappy_ep/src/snappy_ep-install/lib/libsnappy.a
+export BROTLI_STATIC_LIB_ENC=$TRAVIS_BUILD_DIR/parquet-build/arrow_ep-prefix/src/arrow_ep-build/brotli_ep/src/brotli_ep-install/lib/x86_64-linux-gnu/libbrotlienc.a
+export BROTLI_STATIC_LIB_DEC=$TRAVIS_BUILD_DIR/parquet-build/arrow_ep-prefix/src/arrow_ep-build/brotli_ep/src/brotli_ep-install/lib/x86_64-linux-gnu/libbrotlidec.a
+export BROTLI_STATIC_LIB_COMMON=$TRAVIS_BUILD_DIR/parquet-build/arrow_ep-prefix/src/arrow_ep-build/brotli_ep/src/brotli_ep-install/lib/x86_64-linux-gnu/libbrotlicommon.a
+export ZLIB_STATIC_LIB=$TRAVIS_BUILD_DIR/parquet-build/arrow_ep-prefix/src/arrow_ep-build/zlib_ep/src/zlib_ep-install/lib/libz.a
 
 if [ $TRAVIS_OS_NAME == "linux" ]; then
     cmake -DPARQUET_CXXFLAGS=-Werror \
           -DPARQUET_TEST_MEMCHECK=ON \
           -DPARQUET_ARROW_LINKAGE="static" \
+          -DPARQUET_BUILD_SHARED=OFF \
           -DPARQUET_BUILD_BENCHMARKS=ON \
           -DPARQUET_GENERATE_COVERAGE=1 \
           $TRAVIS_BUILD_DIR
 else
     cmake -DPARQUET_CXXFLAGS=-Werror \
           -DPARQUET_ARROW_LINKAGE="static" \
+          -DPARQUET_BUILD_SHARED=OFF \
           $TRAVIS_BUILD_DIR
 fi
