4a09e6181b9f05320562eebc6d135c1a8a979e1b
Wes McKinney <wes.mckinney@twosigma.com>
2017-08-05 12:09:20 -0400

    Upgrade Snappy in MSVC toolchain build to 1.1.6

--- ci/msvc-build.bat
+++ ci/msvc-build.bat
@@ -28,7 +28,7 @@ if NOT "%CONFIGURATION%" == "Debug" (
 )
 
 if "%CONFIGURATION%" == "Toolchain" (
-  conda install -y boost-cpp=1.63 brotli=0.6.0 zlib=1.2.11 snappy=1.1.4 thrift-cpp=0.10.0 -c conda-forge
+  conda install -y boost-cpp=1.63 brotli=0.6.0 zlib=1.2.11 snappy=1.1.6 thrift-cpp=0.10.0 -c conda-forge
 
   set ARROW_BUILD_TOOLCHAIN=%MINICONDA%/Library
   set PARQUET_BUILD_TOOLCHAIN=%MINICONDA%/Library
